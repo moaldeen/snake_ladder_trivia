a263996140a9e0f49b68f7e6283cf6df7c7a16f9
moaldeen <132774635+moaldeen@users.noreply.github.com>
2023-10-21 15:14:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -258,7 +258,7 @@ resetGameBtn.addEventListener('click', resetGame);
 
 let walkSequence = boustrophedonWalk(10, 10);
 const getRandomQuestion = async () => {
-    const apiKey = '';
+    const API_KEY  = '';
     const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
     const data = {
       prompt: 'generate a random trivia question in this format:\n{"question": question,"choices": ["Choice 1", "Choice 2", "Choice 3"],"correctAnswer": "Correct Choice"}',
@@ -267,7 +267,7 @@ const getRandomQuestion = async () => {
     };
     const headers = {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
+      'Authorization': `Bearer ${API_KEY }`
     };
   
     try {
